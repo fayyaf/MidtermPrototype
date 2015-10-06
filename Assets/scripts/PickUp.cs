0883a7bb98b1e1e6339a0b35b08e08cc9024ab60
Fay Koh <fay.koh@nyu.edu>
2015-10-06 13:48:48 -0400

    basic prototype with crates and half-functioning pick up script

--- Assets/scripts/PickUp.cs
+++ Assets/scripts/PickUp.cs
@@ -3,41 +3,54 @@ using System.Collections;
 
 public class PickUp : MonoBehaviour {
 
-	public float distance = 10f;
+	public float distance = 5f;
 	public Transform player;
-	public bool isKinematic;
+	public GameObject cube;
+	public Rigidbody rb;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		distance = Vector3.Distance(player.position, transform.position);
-		
-		if (Input.GetKey(KeyCode.Space) && distance < 2f)
-		{
 
-		}
-
-		if (Input.GetKeyDown (KeyCode.Space))
+		if (Input.GetKeyDown (KeyCode.E))
 		{
+			distance = Vector3.Distance(player.position, transform.position);
+
+			if (distance < 0.2f)
+			{
+				rb.isKinematic = true;
+				rb.detectCollisions = false;
+
+				// parent box to player and set box to kinematic
+				//Sets "newParent" as the new parent of the crate GameObject.
+				cube.transform.SetParent(player);
+				
+				//Same as above, except this makes the player keep its local orientation rather than its global orientation.
+				cube.transform.SetParent(player, false);
+			}
 
 		}
 		else
 		{
-			transform.parent = null;
+			rb.isKinematic = false;
+			rb.detectCollisions = true;
+			transform.parent = null; // drops the box
 		}
 
 		/*
+		 * 
 		 * if (player pressed space)
-    (check distance between box and player)
-        (if distance < 2, then parent box to player and set box to kinematic)
+    		(check distance between box and player)
+        		(if distance < 2, then parent box to player and set box to kinematic)
 
-if (player released space)
-   (unparent box, set box to not kinematic(
+		* if (player released space)
+   			(unparent box, set box to not kinematic(
 		 * 
 		 * 
 		 * 
